62d30ff5456b1b07360c3fd52346dffb78931c99
Vlad Chiovianu <vlad.chiovianu@earthlink.ro>
2023-01-18 18:49:47 +0200

    updates deps; implements native php enums; removes .styleci

--- database/factories/ServiceFactory.php
+++ database/factories/ServiceFactory.php
@@ -3,7 +3,7 @@
 namespace LaravelEnso\Services\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use LaravelEnso\Helpers\Enums\VatRates;
+use LaravelEnso\Helpers\Enums\VatRate;
 use LaravelEnso\MeasurementUnits\Models\MeasurementUnit;
 use LaravelEnso\Services\Models\Service;
 
@@ -18,7 +18,7 @@ class ServiceFactory extends Factory
             'name' => $this->faker->name,
             'code' => $this->faker->unique()->ean8,
             'list_price' => $this->faker->numberBetween(1, 300),
-            'vat_percent' => VatRates::keys()->random(),
+            'vat_percent' => VatRate::random()->value,
             'description' => $this->faker->text,
             'is_active' => true,
         ];
